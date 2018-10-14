401a6386fd73ec9ed2d81949d18dd5ea929d57ac
Antonio Falcao <arfj@edu.univali.br>
2018-10-14 08:30:54 -0300

    CM - Ajustes de execução

--- Desafio1/Program.cs
+++ Desafio1/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Desafio1
 {
     class Program
     {
+        private static readonly HttpClient HttpClient = new HttpClient();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iniciando...");
+
+            Buscar().GetAwaiter().GetResult();
+        }
+
+        private static async Task Buscar()
+        {
+            var htmlTask = HttpClient.GetStringAsync("http://www.hiper.com.br");
+
+            ServicePointManager.ServerCertificateValidationCallback =(sender, certificate, chain, sslPolicyErrors) => true;
+
+            var html = await htmlTask;
+
+            var count = Regex.Matches(html, @"Hiper").Count;
+
+            Console.WriteLine($"Quantidade: {count}");
+
+            Console.Read();
         }
     }
 }
